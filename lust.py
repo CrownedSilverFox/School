5020f72b7ed07efbdaf74bc7701a378e76c64b51
crownedsilverfox <byskillcfpro@gmail.com>
2015-10-02 18:35:04 +0700

    git init

---
+++ lust.py
@@ -0,0 +1,18 @@
+import json
+
+
+students = json.load(open('Students.json'))
+teachers = json.load(open('Teachers.json'))
+#1.1
+print([student["name"] for student in students])
+#1.2
+print([teacher["name"] for teacher in teachers])
+#1.3
+print([(student['name'], student['surname']) for student in students if student['class'] == '6 А'])
+#1.4
+print(list(set([teacher['school'] for teacher in teachers])))
+#1.5
+surnames = [student['surname'] for student in students]
+for surname in list(set([student['surname'] for student in students])):
+    surnames.remove(surname)
+print([(student['name'], student[surname]) for student in students if student['surname'] in surnames])
